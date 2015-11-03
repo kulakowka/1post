683c6aca138900dfe927b4463b5d641ea91cc032
kulakowka <kulakowka@gmail.com>
2015-11-04 02:37:07 +0300

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
 var $ = require('jquery')
 
-$(document).on('submit', 'form[name="logoutForm"]', function () {
-  console.log('logout')
+$(document).on('click', '.js-button-logout', function () {
+  $.post('/api/users/logout', function () {
+    document.location.href = '/'
+  })
+  return false
+})
+
+$(document).on('submit', 'form[name="loginForm"]', function () {
+  var data = $(this).serialize()
+  $.post('/api/users/login', data, function (json) {
+    if (json.user) document.location.href = '/'// + json.user.username
+  })
   return false
 })
