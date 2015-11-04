aa7269668a1ee42c6743cdfd7b2d1bab26f30db4
kulakowka <kulakowka@gmail.com>
2015-11-04 07:31:46 +0300

    cache

--- config/static.js
+++ config/static.js
@@ -1,4 +1,5 @@
 var express = require('express')
 var path = require('path')
+// var oneDay = 60000
 
-module.exports = express.static(path.resolve(__dirname, '../public'))
+module.exports = express.static(path.resolve(__dirname, '../public')) // , { maxAge: oneDay }
