fb9a74eb6221f6ef2bdd4eb32d535e5af81db541
kulakowka <kulakowka@gmail.com>
2015-11-16 03:52:13 +0300

    fix

--- app/assets/js/helpers/dropdown.js
+++ app/assets/js/helpers/dropdown.js
@@ -5,7 +5,11 @@ module.exports.onClickHandler = onClickHandler
 function onClickHandler () {
   var handler = $(this)
   var dropdown = handler.closest('.dropdown')
-  $('.dropdown.open').removeClass('open')
-  dropdown.toggleClass('open')
+  if (dropdown.hasClass('open')) {
+    dropdown.removeClass('open')
+  } else {
+    $('.dropdown.open').removeClass('open')
+    dropdown.addClass('open')
+  }
   return false
 }
